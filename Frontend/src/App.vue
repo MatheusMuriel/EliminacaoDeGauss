47037a5b3e4e5963960cf2466922cdf6613d94f9
Matheus Muriel <matheus.muriel@outlook.com>
2019-10-19 17:07:21 -0300

    Processamento de dados da matriz LU

--- Frontend/src/App.vue
+++ Frontend/src/App.vue
@@ -25,6 +25,7 @@
           
           <b-row>
             <b-button variant="outline-primary" @click="pegarDados">Calcular</b-button>
+            <b-button variant="outline-primary" @click="testeresponse">Calcular-dev</b-button>
           </b-row>
 
           <b-row>
@@ -59,6 +60,11 @@ export default {
     // this.$on('valoresACalcular', this.calcular)
   },
   methods: {
+    testeresponse () {
+      let respontaRequestFixa = '$$$1###[[1.0, 2.0, 3.0], [0.0, -3.0, -4.0], [3.0, 2.0, 1.0]]&&&[9.0, -11.0, 6.0]$$$2###[[1.0, 2.0, 3.0], [0.0, -3.0, -4.0], [0.0, -4.0, -8.0]]&&&[9.0, -11.0, -21.0]$$$3###[[1.0, 2.0, 3.0], [0.0, -3.0, -4.0], [0.0, 0.0, -2.666666666666667]]&&&[9.0, -11.0, -6.333333333333334]§§§[0.875, 0.5, 2.375]§§§[[1, 0, 0], [2, 1, 0], [3, 1, 1]]§§§[[1.0, 2.0, 3.0], [0.0, -3.0, -4.0], [0.0, 0.0, -2.666666666666667]]'
+
+      this.processarResposta(respontaRequestFixa)
+    },
     gerarMatriz () {
       let tamanhoMatriz = Number(this.tamanho)
 
@@ -90,18 +96,6 @@ export default {
 
       this.gerartabNav()
     },
-    gerartabNav () {
-
-      let navMatrizClass = Vue.extend(NavMatriz);
-      let instanciaNavMatriz = new navMatrizClass({
-        propsData: {listaItens: this.itemsMatriz}
-      });
-      instanciaNavMatriz.$mount();
-
-      let rowNav = this.$refs.nav;
-
-      rowNav.appendChild(instanciaNavMatriz.$el)
-    },
     pegarDados () {
       this.instanciaMatriz.$emit('pegarValores')
       let mapatriz = this.instanciaMatriz.$data.mapatriz
@@ -129,18 +123,25 @@ export default {
           console.log(error)
         })
     },
+    exibirMatrizLU () {
+      // TODO
+    },
     processarResposta (dados) {
+      // str_passos + "§§§" + matriz_x + "§§§" + matriz_l + "§§§" + matriz_u
       let itens = dados.split("§§§")
 
-      let matriz_x = JSON.parse(itens[0])
-      let strPassos = itens[1]
+      console.table(itens)
 
-      let passos_iteracoes = strPassos.split("$$$")
-      passos_iteracoes.shift()
+      let strPassos = itens[0]
+      let strMatrizX = itens[1]
+      let strMatrizL = itens[2]
+      let strMatrizU = itens[3]
+      
+      let passosIteracoes = strPassos.split("$$$")
+          passosIteracoes.shift()
 
       let passos = new Map()
-
-      passos_iteracoes.forEach((strPasso) => {
+      passosIteracoes.forEach((strPasso) => {
         let arrPasso = strPasso.split("###")
         let numPasso = arrPasso[0]
         let strAB = arrPasso[1].split("&&&")
@@ -153,7 +154,21 @@ export default {
 
         passos.set(numPasso, [arrA, arrB])
       })
-      console.log(matriz_x, passos)
+      // O resultado é o ultimo item de passos
+
+      let matrizX = JSON.parse(strMatrizX)
+
+      let matrizL = JSON.parse(strMatrizL)
+      let matrizU = JSON.parse(strMatrizU)
+
+
+      console.log('Passos ')
+      console.log(passos)
+
+      console.log('Matriz X', matrizX)
+
+      console.log('Matriz L', matrizL)
+      console.log('Matriz U', matrizU)
     }
   }
 }
